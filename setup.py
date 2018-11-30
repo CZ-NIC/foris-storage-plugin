76357b68d1e225cf05cce0e0b7865efd93e15b99
Stepan Henek <stepan.henek@nic.cz>
2018-11-30 16:58:18 +0100

    version bump

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ class BuildCmd(build_py):
 
 setup(
     name="Foris Storage Plugin",
-    version="1.2",
+    version="1.3",
     description="Storage plugin for foris web interfce",
     author="CZ.NIC, z. s. p. o.",
     author_email="stepan.henek@nic.cz",
