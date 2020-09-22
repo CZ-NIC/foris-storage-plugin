2855be18882ff87bd5611605746d79f99bf0886b
Martin Matějek <martin.matejek@nic.cz>
2020-09-22 23:58:51 +0200

    version 3.0

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ class BuildCmd(build_py):
 
 setup(
     name="Foris Storage Plugin",
-    version="2.0",
+    version="3.0",
     description="Storage plugin for foris web interfce",
     author="CZ.NIC, z. s. p. o.",
     author_email="packaging@turris.cz",
